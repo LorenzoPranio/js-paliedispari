d45f99234da75dfb8c427eb0979a2c211df68a78
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-04 16:24:52 +0200

    [UPD]Added scaffolding in script.js in js folder in pari e dispari folder

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,17 @@
+//Creo un promtp per chiedere all'utente "Pari o Dispari?"
+
+//Creo un prompt per chiedere un numero all'utente con il parsInt
+
+//IF il nuemro che hai scelto è pari e la somma è pari, e se il numero che hai scelto è dispari e la somma è dispari: Hai vinto!
+
+//ELSE: Hai perso!
+
+//Creo una variabile per il numero del pc
+
+//Creo una funzione per generare un numero da 1 a 5
+
+//Creo una funzione per comparare i numeri
+
+//Creo la variabile per sommare i numeri
+
+//IF la somma non è divisibile per 2, non è pari

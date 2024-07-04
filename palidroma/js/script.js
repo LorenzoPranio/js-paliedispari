17769fb35f9584405589be0c52d8c7884e67aa86
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-04 16:04:31 +0200

    [UPD]Added function reverse_string in script.js in palidroma folder

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -1,17 +1,22 @@
 //Chiedo all'utente di inserire una parola con un prompt
 
 let parola = prompt('Inserisci una parola');
+let parola_reverse
 
 //Dichiaro una funzione con parametro "parola"
-
-
-
 //Divido l'input in singole lettere e Inverto l'ordine delle lettere
 //Unisco in una stringa le lettere per generare una nuova parola
 
+function reverse_string(parola){
+    parola_reverse = parola.split('').reverse().join('')
+    //IF la nuova parola è identica alla vecchia la mostro in console.log
+    
+    //ELSE Mostro in console "parola non polidroma"
+}
 
 
 
-//IF la nuova parola è identica alla vecchia la mostro in console.log
 
-//ELSE Mostro in console "parola non polidroma"
+
+
+

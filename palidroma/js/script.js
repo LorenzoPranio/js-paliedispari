0ed65bc365812e3fadbbafaaf926a96881029e31
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-04 17:18:49 +0200

    [FIX]Start excercise for cicle for

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -1,13 +1,13 @@
 //Chiedo all'utente di inserire una parola con un prompt
 
-let parola = prompt('Inserisci una parola');
-let parola_reverse
+//let parola = prompt('Inserisci una parola');
+//let parola_reverse
 
 //Dichiaro una funzione con parametro "parola"
 //Divido l'input in singole lettere e Inverto l'ordine delle lettere
 //Unisco in una stringa le lettere per generare una nuova parola
 
-function reverse_string(parola){
+/*function reverse_string(parola){
     parola_reverse = parola.split('').reverse().join('')
     //IF la nuova parola è identica alla vecchia la mostro in console.log
     if(parola === parola_reverse){
@@ -17,9 +17,19 @@ function reverse_string(parola){
     else{
         alert('La parola non è polidroma')
     }
+}*/
+//reverse_string(parola)
+//CON CICLO FOR
+
+let parola = prompt('Inserisci una parola');
+
+let parola_reverse = ('')
+
+for (let i = parola.length - 1; i == 0; i-- ){
 }
 
-reverse_string(parola)
+console.log(parola_reverse)
+
 
 
 

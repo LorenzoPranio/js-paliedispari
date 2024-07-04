50de59fc47495f3e24f7a8f8ca9b6abb9a266594
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-04 15:10:38 +0200

    [UPD]Added scaffolding in script.js in js folder in polidroma folder

---
+++ palidroma/js/script.js
@@ -0,0 +1,13 @@
+//Chiedo all'utente di inserire una parola con un prompt
+
+//Dichiaro una funzione con parametro "parola"
+
+//Divido l'input in singole lettere mettendole in un array
+
+//Inverto l'ordine delle lettere
+
+//Unisco in una stringa le lettere per generare una nuova parola
+
+//IF la nuova parola è identica alla vecchia la mostro in console.log
+
+//ELSE Mostro in console "parola non polidroma"
